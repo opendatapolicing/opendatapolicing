91f9f92360cc909defc7abadf97659f40fd67986
ctate <computate@computate.org>
2021-01-18 10:20:45 -0700

    Adding the important Wrap class.

---
+++ src/main/java/com/opendatapolicing/enus/wrap/Wrap.java
@@ -0,0 +1,68 @@
+package com.opendatapolicing.enus.wrap;
+
+import java.io.Serializable;
+import java.util.ArrayList;
+
+/**
+ *  **/
+
+public class Wrap<DEV> implements Serializable {
+
+	public String var;
+
+	public Wrap<DEV> var(String o) {
+		var = o;
+		return this;
+	}
+
+	public DEV o;
+
+	public Wrap<DEV> o(DEV o) {
+		this.o = o;
+		return this;
+	}
+
+	public Class<?> c;
+
+	public Wrap<DEV> c(Class<?> o) {
+		c = o;
+		return this;
+	}
+
+	public Object p;
+
+	public Wrap<DEV> p(Object o) {
+		p = o;
+		return this;
+	}
+
+	public Class<?> cChild;
+
+	public Wrap<DEV> cChild(Class<?> o) {
+		cChild = o;
+		return this;
+	}
+
+	public Boolean alreadyInitialized = false;
+
+	public Wrap<DEV> alreadyInitialized(Boolean o) {
+		alreadyInitialized = o;
+		return this;
+	}
+
+	public Object siteRequestObject;
+
+	public Wrap<DEV> siteRequestObject(Object o) {
+		siteRequestObject = o;
+		return this;
+	}
+
+	public ArrayList<String> pageVars = new ArrayList<String>();
+
+	public void  pageVarsAdd(String...pageVars) {
+		for(String pageVar : pageVars)
+			this.pageVars.add(pageVar);
+	}
+}
+
+
